878579b8794bf7685ea1447b02dea7486f7b997d
Camilo <jcamilovelandiab@gmail.com>
2022-04-17 21:56:58 -0500

    Remove blacklist and use whitelist instead. We only need the cart reducer to be persisted.

--- src/store/store.js
+++ src/store/store.js
@@ -19,7 +19,7 @@ const composeEnhancer =
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);

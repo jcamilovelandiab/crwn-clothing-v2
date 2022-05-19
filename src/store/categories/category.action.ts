491658e8faa8e41eed5a3b65c5ab5ca7b1915bef
Camilo <jcamilovelandiab@gmail.com>
2022-05-18 22:40:57 -0500

    Setting up typescript and migrating category reducer to typescript

---
+++ src/store/categories/category.action.ts
@@ -0,0 +1,33 @@
+import { Dispatch } from 'redux';
+
+import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
+import { createAction, Action, ActionWithPayload } from '../../utils/reducer/reducer.utils';
+
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, Category[]>;
+export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, Error>;
+export type CategoryAction = 
+  | FetchCategoriesStart
+  | FetchCategoriesSuccess
+  | FetchCategoriesFailed;
+
+export const fetchCategoriesStart = (): FetchCategoriesStart =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray: Category[]): FetchCategoriesSuccess =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+
+export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async(dispatch: Dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesArray: any = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error: unknown) {
+    dispatch(fetchCategoriesFailed(<Error>error));
+  }
+}

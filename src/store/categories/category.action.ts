e854559689639e2a625ceb523eea8261b6f87bc0
Camilo <jcamilovelandiab@gmail.com>
2022-05-19 21:10:46 -0500

    Making category reducer type safe withMatcher overloading.

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,28 +1,34 @@
 import { Dispatch } from 'redux';
 
 import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
-import { createAction, Action, ActionWithPayload } from '../../utils/reducer/reducer.utils';
+import { createAction, Action, ActionWithPayload, withMatcher } from '../../utils/reducer/reducer.utils';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
 export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
 export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, Category[]>;
 export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, Error>;
-export type CategoryAction = 
+export type CategoryAction =
   | FetchCategoriesStart
   | FetchCategoriesSuccess
   | FetchCategoriesFailed;
 
-export const fetchCategoriesStart = (): FetchCategoriesStart =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesStart = withMatcher(
+  (): FetchCategoriesStart =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+);
 
-export const fetchCategoriesSuccess = (categoriesArray: Category[]): FetchCategoriesSuccess =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+export const fetchCategoriesSuccess = withMatcher(
+  (categoriesArray: Category[]): FetchCategoriesSuccess =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray)
+);
 
-export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher(
+  (error: Error): FetchCategoriesFailed =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+);
 
-export const fetchCategoriesAsync = () => async(dispatch: Dispatch) => {
+export const fetchCategoriesAsync = () => async (dispatch: Dispatch) => {
   dispatch(fetchCategoriesStart());
   try {
     const categoriesArray: any = await getCategoriesAndDocuments();
